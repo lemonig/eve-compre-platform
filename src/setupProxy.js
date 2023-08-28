14dab40380d6767ac686ed08e150c568240ed88b
liweiqiang <liweiqiang@grean.com.cn>
2023-08-28 09:26:56 +0800

    file

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function (app) {
   app.use(
     "/api", // 指定需要转发的请求
     createProxyMiddleware({
-      // target: "http://192.168.188.103:8080", //服务器的地址
-      target: "http://192.168.168.9:15713/", //服务器的地址
+      target: "http://192.168.188.103:8080", //服务器的地址
+      // target: "http://192.168.168.9:15713/", //服务器的地址
       // target: "http://127.0.0.1:4523/m1/1850620-0-default/", //mock
       // target: "https://portal.greandata1.com/", //服务器的地址
       changeOrigin: true,
