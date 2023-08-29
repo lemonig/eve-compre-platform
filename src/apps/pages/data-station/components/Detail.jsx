a3f438ad2f66318cbce56978edc1439b2be58b54
liweiqiang <liweiqiang@grean.com.cn>
2023-08-29 15:47:54 +0800

    feat:站点详情增加问价预览

--- src/apps/pages/data-station/components/Detail.jsx
+++ src/apps/pages/data-station/components/Detail.jsx
@@ -1,14 +1,9 @@
 import React, { useState, useEffect } from "react";
-import {
-  Col,
-  Row,
-  Divider,
-  Drawer,
-} from "antd";
+import { Col, Row, Divider, Drawer } from "antd";
 import IconFont from "@Components/IconFont";
-import '../index.less'
+import "../index.less";
 import { stationInfo } from "@Api/data_info.js";
-
+import { a } from "react-spring";
 
 const DescriptionItem = ({ title, content }) => (
   <div className="site-description-item-profile-wrapper">
@@ -19,22 +14,27 @@ const DescriptionItem = ({ title, content }) => (
 
 function Detail({ record, open, closeModal }) {
   const [loading, setLoading] = useState(false);
-  const [data, setData] = useState([])
-
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    getDetail()
+    getDetail();
   }, []);
 
   const getDetail = async () => {
     let { data } = await stationInfo({
       id: record.id,
     });
-    setData(data)
+    setData(data);
   };
 
   return (
-    <Drawer width={640} placement="right" closable={false} onClose={closeModal} open={open}>
+    <Drawer
+      width={640}
+      placement="right"
+      closable={false}
+      onClose={closeModal}
+      open={open}
+    >
       <p
         className="site-description-item-profile-p"
         style={{
@@ -43,26 +43,42 @@ function Detail({ record, open, closeModal }) {
       >
         站点详情
       </p>
-      {
-        data.map(item => {
-          return <>
+      {data.map((item) => {
+        return (
+          <>
             <p className="site-description-item-profile-p">{item.name}</p>
             <Row>
-              {
-                item.list.map(jtem => {
-                  return <Col span={12}>
+              {item.list.map((jtem, index) => {
+                const key = `${jtem.name}-${index}`; // Create a unique key
+
+                if (jtem.name === "附件") {
+                  return (
+                    <Col key={key} span={12}>
+                      <DescriptionItem
+                        title={jtem.name}
+                        content={jtem.value.map((ztem) => (
+                          <a key={ztem.name} href={ztem.value}>
+                            {ztem.name}
+                          </a>
+                        ))}
+                      />
+                    </Col>
+                  );
+                }
+
+                return (
+                  <Col key={key} span={12}>
                     <DescriptionItem title={jtem.name} content={jtem.value} />
                   </Col>
-                })
-              }
+                );
+              })}
             </Row>
+
             <Divider />
           </>
-        })
-      }
-
+        );
+      })}
     </Drawer>
-
   );
 }
 
